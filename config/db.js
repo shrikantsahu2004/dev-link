bb6542441f320af7695d17469956ab4473aca954
shrikantsahu2004 <sahu.sr@somaiya.edu>
2021-11-20 21:01:16 +0530

    User Auth using jwt added

--- config/db.js
+++ config/db.js
@@ -1,19 +1,17 @@
-const mongoose = require("mongoose")
-const config = require("config")
-const db = config.get('mongoURI')
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
 
-const connectDB = async ()=>{
-    try{
-        //mongodb connection may take some time hence async await
-        await mongoose.connect(db,{useNewUrlParser : true})
-        console.log("MongoDB Connected")
-
-    }catch(err){
-
-        console.log(err)
-        //Exit process with failure
-        process.exit(1)
-    }
-}
+const connectDB = async () => {
+  try {
+    //mongodb connection may take some time hence async await
+    await mongoose.connect(db, { useNewUrlParser: true });
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.log(err);
+    //Exit process with failure
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
---
+++ routes/api/users.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const gravatar = require("gravatar");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const User = require("../../models/User.js");
+const config = require("config");
+// @route: POST api/users
+// @access: Public
+router.post(
+  "/",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    let { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
+      }
+
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
+
+      const salt = bcrypt.genSaltSync(10);
+      password = bcrypt.hashSync(password, salt);
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          console.log(token);
+          return res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
+
+module.exports = router;
